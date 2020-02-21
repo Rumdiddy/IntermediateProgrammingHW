55021c7a33ca0c2edd770e208b74e8066012fa94
Sam Jin <sjin16@jhu.edu>
2020-02-21 09:57:05 -0500

    Completed pattern_match function

--- homework/hw3/dnasearch.c
+++ homework/hw3/dnasearch.c
@@ -8,5 +8,17 @@
 //Returns the first pattern offset beginning at start_at. Returns -1
 //if there is no pattern found.
 int pattern_match(const char t[], int tlen, const char p[], int plen, int start_at) {
-  
+  int match;
+  for(int i = start_at; i <= (tlen - plen); i++) {
+    match = 1;
+    for(int j = 0; j <= plen; j++) {
+      if (p[j] != t[i + j]) {
+	match = 0;
+	break;
+      }
+    }
+    if (match != 0) {
+      return i;
+    }     
+  }
 }
