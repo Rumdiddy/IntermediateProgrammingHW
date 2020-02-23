cbca2f1011f483e7530987d1c85e1ac5d3339648
Sam Jin <sjin16@jhu.edu>
2020-02-22 19:31:48 -0500

    Added total offset function to header

--- homework/hw3/dnasearch.h
+++ homework/hw3/dnasearch.h
@@ -7,3 +7,8 @@
 //Returns -1 if no such occurence. *Similar to Java indexOf()*
 int pattern_match(const char t[], int tlen, const char p[], int plen, int start_at);
 
+//Utilizes pattern_match() to find all occurrences of pattern.
+//Returns array of offsets.
+int * total_offset(const char t[], const char p[]);
+
+
