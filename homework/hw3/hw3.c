8c4db9fda0ad8180554bf83ae583ced4d24bdc2d
Sam Jin <sjin16@jhu.edu>
2020-02-27 18:20:21 -0500

    added beginning of hw3.c

--- homework/hw3/hw3.c
+++ homework/hw3/hw3.c
@@ -4,3 +4,21 @@
 
 //Implements functions and forms user interface.
 #include "dnasearch.h"
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    printf("Please enter a file name in command line");
+    return 1; //indicate error
+  }
+  char seqarray[15001];
+  
+  char *fname = argv[1];
+  FILE* input = fopen(fname, "r");
+
+  if (input == NULL) {
+    printf("Error: could not open input file\n");
+    return 1; //indicate error
+  }
+
+  
