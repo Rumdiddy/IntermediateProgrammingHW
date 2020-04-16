c38b4891b39df52e6a4c76b0b737c13d31b6f567
Sam Jin <sjin16@jhu.edu>
2020-04-16 03:56:45 -0400

    Completed until == operator function

--- homework/hw6/CTree.cpp
+++ homework/hw6/CTree.cpp
@@ -2,10 +2,12 @@
 //sjin16
 //CTree.cpp
 
+#include <cstdlib>
 #include <iostream>
 #include "CTree.h"
+#include <string>
 
-using std::ostream;
+using std::ostream; using std::string;
 
 //Constructor function that takes in character for node.
 CTree::CTree(char ch) {
@@ -26,14 +28,23 @@ bool CTree::addChild(char ch) {
     return kids->addSibling(ch);
 }
 
+//Overloaded function of addChild. Passed root pointer
+bool CTree::addChild(CTree * root) {
+    return kids->addSibling(root); 
+}
+  
 //Function to add a sibling. Passed character data for sibling.
 bool CTree::addSibling(char ch) {
+
+  //Ensure valid addition
   if (data == ch) {
     return false;
   }
   if (prev == NULL) {
     return false;
   }
+
+  //Adds node based on case
   if (ch < data) {
     CTree * temproot = prev;
     prev = new CTree(ch);
@@ -55,8 +66,87 @@ bool CTree::addSibling(char ch) {
       sibs->prev->sibs = sibs;
       sibs = sibs->prev;
       return true;
-    }
-    if (sibs->data < ch) {
+    } else {
       return sibs->addSibling(ch);
     }
+  }
 }
+
+//Function that adds new root node (along with its children) as an existing sibling
+bool CTree::addSibling(CTree * root) {
+
+  //Ensure not parent node or root is parent node
+  if (data == root->data) {
+    return false;
+  }
+  if (prev == NULL) {
+    return false;
+  }
+  if (root->sibs != NULL) {
+    return false;
+  }
+  if (root->prev != NULL) {
+    return false;
+  }
+
+  //Add node as sibling
+  if (root->data < data) {
+    CTree * tempr = prev;
+    prev = root;
+    prev->sibs = this;
+    tempr->kids = prev;
+    prev->prev = tempr;
+    return true;
+  } else {
+    if (sibs == NULL) {
+      sibs = root;
+      sibs->prev = this;
+      return true;
+    }
+    if (sibs->data == root->data) {
+      return false;
+    }
+    if(sibs->data > ch) {
+      sibs->prev = root;
+      sibs->prev->prev = this;
+      sibs->prev->sibs = sibs;
+      sibs = sibs->prev;
+      return true;
+    } else {
+      return sibs->addSibling(root);
+    }
+  }
+}
+
+//Overloaded operator function '^' to addChild
+CTree& CTree::operator^(CTree& rt) {
+  addChild(&rt);
+  return *this;
+}
+
+//Creates a string of all characters, separated by new lines.
+string CTree::toString() {
+  string temp;
+  
+  //Ensure start at parent node
+  if (prev != NULL) {
+    return prev->toString();
+  }
+
+  //Recursion going through kids then sibs
+  if (kids != NULL) {
+    temp = (string(1, this->data) + "\n");
+    return temp.append(kids->toString());
+  }
+  if (sibs != NULL) {
+    temp = (string(1, this->data) + "\n");
+    return temp.append(sibs->toString());
+  }
+}
+
+//Overloaded << operator function that displays the same value as toString()
+ostream& operator<<(ostream& os, CTree& rt) {
+  os << rt.toString();
+  return os;
+}
+
